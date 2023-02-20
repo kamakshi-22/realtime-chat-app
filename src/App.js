ee8440c1e97b58f67366ef0bb35ed75339db10bb
Kamakshi Karakoti <kamakshikarakoti22@gmail.com>
2023-02-20 08:38:17 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<h1 >Home</h1>}/>
           <Route path="about" element={<h1>About</h1>} />
           <Route path="contact" element={<h1>Contact</h1>} />
           <Route path="*" element={<h1>404</h1>} />
